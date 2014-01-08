9151886e00bf49a8874570925f75c9fa7f280b62
Hang Chen <hangisnice@gmail.com>
2014-01-09 00:31:06 +0800

    Updated the LICENSE info
    Updated the LICENSE info

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
-TwitterCover
-============
+## TwitterCover ##
 
-TwitterCover is a parallax top view to any UIScrollView, inspired by Twitter for iOS
+TwitterCover is a parallax top view to any UIScrollView, inspired by Twitter for iOS.
+
+Completely created using UIKit framework.
+
+Easy to drop into your project.
+
+You can add this feature to your own project, `TwitterCover` is easy-to-use. 
+
+
+## Requirements ##
+
+TwitterCover requires Xcode 5, targeting either iOS 5.0 and above, ARC-enabled.
+
+
+## How to use ##
+	
+Drag UIScrollView+TwitterCover.h amd UIScrollView+TwitterCover.m files to your project. 
+
+No other frameworks required.
+
+    #import "UIScrollView+TwitterCover.h"
+
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    [scrollView addTwitterCoverWithImage:[UIImage imageNamed:@"cover.png"]];
+
+    
+## How it looks ##
+
+![TwitterCover] (https://raw.github.com/cyndibaby905/TwitterCover/master/TwitterCover.gif)
+
+
+## Lincense ##
+
+`TwitterCover` is available under the MIT license. See the LICENSE file for more info.
+
--- TwitterCover/CHViewController.m
+++ TwitterCover/CHViewController.m
@@ -3,8 +3,25 @@
 //  TwitterCover
 //
 //  Created by hangchen on 1/7/14.
-//  Copyright (c) 2014 hangchen. All rights reserved.
+//  Copyright (c) 2014 Hang Chen (https://github.com/cyndibaby905)
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 
 #import "CHViewController.h"
 #import "UIScrollView+TwitterCover.h"
@@ -27,9 +44,8 @@
     [scrollView addSubview:({
         UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20, CHTwitterCoverViewHeight, self.view.bounds.size.width - 40, 600 - CHTwitterCoverViewHeight)];
         label.numberOfLines = 0;
-        label.font = [UIFont systemFontOfSize:20];
-        label.text = @"TwitterCover is a parallax top view to any UIScrollView, inspired by Twitter for iOS.\n\nTwitterCover is a parallax top view to any UIScrollView, inspired by Twitter for iOS.\n";
-        
+        label.font = [UIFont systemFontOfSize:22];
+        label.text = @"TwitterCover is a parallax top view to any UIScrollView, inspired by Twitter for iOS.\n\nCompletely created using UIKit framework.\n\nEasy to drop into your project.\n\nYou can add this feature to your own project, TwitterCover is easy-to-use.";
         label;
     })];
 
